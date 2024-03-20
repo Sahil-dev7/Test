97b53c1a8264f5b953b1c2e89b5e3f35e0f52188
Sahil Wadhwani <techtheera27@gmail.com>
2024-03-20 08:41:20 +0000

    fix: Resolved bug in Student

--- Java/Student.java
+++ Java/Student.java
@@ -1,4 +1,4 @@
 // Auto-generated Java file
-// Work on Student at 2024-03-10 08:41:20
+// Work on Student at 2024-03-20 08:41:20
 public class Student {
  public static void main(String[] a) { System.out.println("{topic}"); } }
