fb0aad73d17171b733292aa9dd91791851154144
Sahil Wadhwani <techtheera27@gmail.com>
2024-04-05 08:35:42 +0000

    refactor: Improved Student structure

--- Java/Student.java
+++ Java/Student.java
@@ -1,4 +1,4 @@
 // Auto-generated Java file
-// Work on Student at 2024-03-07 08:35:42
+// Work on Student at 2024-04-05 08:35:42
 public class Student {
  public static void main(String[] a) { System.out.println("{topic}"); } }
