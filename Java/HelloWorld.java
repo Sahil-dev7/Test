1f38772d1611b19971c53669562715113a064dde
Sahil Wadhwani <techtheera27@gmail.com>
2024-04-14 08:02:18 +0000

    test: Added tests for HelloWorld

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,4 +1,4 @@
 // Auto-generated Java file
-// Work on HelloWorld at 2024-04-11 08:02:18
+// Work on HelloWorld at 2024-04-14 08:02:18
 public class HelloWorld {
  public static void main(String[] a) { System.out.println("{topic}"); } }
