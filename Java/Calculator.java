d48542e692029126f59d131fa8733701ac2793b1
Sahil Wadhwani <techtheera27@gmail.com>
2024-05-03 08:41:20 +0000

    refactor: Improved Calculator structure

--- Java/Calculator.java
+++ Java/Calculator.java
@@ -1,4 +1,4 @@
 // Auto-generated Java file
-// Work on Calculator at 2024-04-28 08:41:20
+// Work on Calculator at 2024-05-03 08:41:20
 public class Calculator {
  public static void main(String[] a) { System.out.println("{topic}"); } }
