eb4889dd92cceda77dd57d4ec1818b24559562d2
Sahil Wadhwani <techtheera27@gmail.com>
2024-06-07 08:41:20 +0000

    refactor: Improved HelloWorld structure

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,4 +1,4 @@
 // Auto-generated Java file
-// Work on HelloWorld at 2024-05-25 08:41:20
+// Work on HelloWorld at 2024-06-07 08:41:20
 public class HelloWorld {
  public static void main(String[] a) { System.out.println("{topic}"); } }
