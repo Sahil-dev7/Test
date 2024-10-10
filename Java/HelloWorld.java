55bdc92457aed622e127fbcf56932beb10c7c4d8
Sahil Wadhwani <techtheera27@gmail.com>
2024-10-10 08:41:20 +0000

    fix: Resolved bug in HelloWorld

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -1,4 +1,4 @@
 // Auto-generated Java file
-// Work on HelloWorld at 2024-08-31 08:41:20
+// Work on HelloWorld at 2024-10-10 08:41:20
 public class HelloWorld {
  public static void main(String[] a) { System.out.println("{topic}"); } }
