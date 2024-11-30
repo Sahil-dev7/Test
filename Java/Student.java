19b5f85fa9f1d53b98a9a5b4ef43af504c077d2f
Sahil Wadhwani <techtheera27@gmail.com>
2024-11-30 08:41:20 +0000

    feat: Added Student

--- Java/Student.java
+++ Java/Student.java
@@ -1,4 +1,4 @@
 // Auto-generated Java file
-// Work on Student at 2024-11-29 08:41:20
+// Work on Student at 2024-11-30 08:41:20
 public class Student {
  public static void main(String[] a) { System.out.println("{topic}"); } }
