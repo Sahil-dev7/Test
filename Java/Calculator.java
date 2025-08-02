a1224c093c8afa80ea81c77ab669035240f763b7
Sahil Wadhwani <techtheera27@gmail.com>
2025-08-02 08:37:49 +0000

    fix: Resolved bug in Calculator

--- Java/Calculator.java
+++ Java/Calculator.java
@@ -1,4 +1,4 @@
 // Auto-generated Java file
-// Work on Calculator at 2025-06-13 08:37:49
+// Work on Calculator at 2025-08-02 08:37:49
 public class Calculator {
  public static void main(String[] a) { System.out.println("{topic}"); } }
